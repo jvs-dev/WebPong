aa4148a975d2945e64163e44a804640f32a4a8ed
joão vitor <shepherdcom12@gmail.com>
2023-11-22 11:44:28 -0300

    fix players position

--- src/components/ball/script.js
+++ src/components/ball/script.js
@@ -10,7 +10,7 @@ function goBall() {
     let positionY = 0
     let Yvelocity = Math.floor(Math.random() * (5 - (-5) + 1)) + (-5);
     let Xvelocity = 5
-    let numeroAleatorioX = 2 /* Math.floor(Math.random() * 2) + 1; */
+    let numeroAleatorioX = Math.floor(Math.random() * 2) + 1;
     let gameSection = document.getElementById("game")
     let sectionHeight = gameSection.clientHeight;
     let sectionWidth = gameSection.clientWidth;
@@ -18,8 +18,9 @@ function goBall() {
 
 
     switch (numeroAleatorioX) {
-        case 2:
-            let ballInterval = setInterval(() => {
+        case 1:
+            Xvelocity = -5
+            let ballInterval1 = setInterval(() => {
                 if (positionX < Number(`${(sectionWidth / 2) - 20}`) && positionX > Number(`-${(sectionWidth / 2) - 20}`) && positionY > Number(`-${(sectionHeight / 2) - 25}`) && positionY < Number(`${(sectionHeight / 2) - 25}`)) {
                     ball.style.transform = `translate(${positionX + Xvelocity}px, ${positionY + Yvelocity}px)`
                     positionX = positionX + Xvelocity
@@ -37,7 +38,7 @@ function goBall() {
                                     multiplys++
                                 }
                             } else {
-                                clearInterval(ballInterval)
+                                clearInterval(ballInterval1)
                                 LoseWin("WIN")
                                 gameSection.classList.remove("light")
                             }
@@ -58,7 +59,60 @@ function goBall() {
                                     multiplys++
                                 }
                             } else {
-                                clearInterval(ballInterval)
+                                clearInterval(ballInterval1)
+                                LoseWin("LOSE")
+                                gameSection.classList.remove("light")
+                            }
+                        }
+                    }
+                    if (positionY < Number(`-${(sectionHeight / 2) - 25}`) || positionY > Number(`${(sectionHeight / 2) - 25}`)) {
+                        Yvelocity = Yvelocity * (-1)
+                        positionY = positionY + Yvelocity
+                    }
+                }
+            }, 10)
+            break;
+        case 2:
+            let ballInterval2 = setInterval(() => {
+                if (positionX < Number(`${(sectionWidth / 2) - 20}`) && positionX > Number(`-${(sectionWidth / 2) - 20}`) && positionY > Number(`-${(sectionHeight / 2) - 25}`) && positionY < Number(`${(sectionHeight / 2) - 25}`)) {
+                    ball.style.transform = `translate(${positionX + Xvelocity}px, ${positionY + Yvelocity}px)`
+                    positionX = positionX + Xvelocity
+                    positionY = positionY + Yvelocity
+                } else {
+                    if (positionX > Number(`${(sectionWidth / 2) - 20}`) || positionX < Number(`-${(sectionWidth / 2) - 20}`)) {
+                        if (positionX > 0) {
+                            let player2PositionY = player2.getBoundingClientRect();
+                            let ballPositionY = ball.getBoundingClientRect()
+                            if (player2PositionY.top - (player2.clientHeight / 3) <= ballPositionY.top && player2PositionY.top + player2.clientHeight >= ballPositionY.top) {
+                                Xvelocity = Xvelocity * (-1)
+                                positionX = positionX + Xvelocity
+                                if (multiplys < 35) {
+                                    Xvelocity = Xvelocity * 1.05
+                                    multiplys++
+                                }
+                            } else {
+                                clearInterval(ballInterval2)
+                                LoseWin("WIN")
+                                gameSection.classList.remove("light")
+                            }
+
+                        } else if (positionX < 0) {
+                            let player1PositionY = player1.getBoundingClientRect();
+                            let ballPositionY = ball.getBoundingClientRect()
+                            if (player1PositionY.top - (player1.clientHeight / 3) <= ballPositionY.top && player1PositionY.top + player1.clientHeight >= ballPositionY.top) {
+                                Xvelocity = Xvelocity * (-1)
+                                positionX = positionX + Xvelocity
+                                scoreNumber++
+                                score.innerHTML = `Pontos ${scoreNumber}`
+                                if (scoreNumber >= 35) {
+                                    gameSection.classList.add("light")
+                                }
+                                if (multiplys < 35) {
+                                    Xvelocity = Xvelocity * 1.05
+                                    multiplys++
+                                }
+                            } else {
+                                clearInterval(ballInterval2)
                                 LoseWin("LOSE")
                                 gameSection.classList.remove("light")
                             }
--- src/components/players/script.js
+++ src/components/players/script.js
@@ -2,7 +2,7 @@ function player1Comands() {
     let player1 = document.getElementById("player1")
     window.addEventListener('mousemove', (event) => {
         let y = event.clientY;
-        player1.style.top = `${y}px`
+        player1.style.top = `${y - Number(player1.clientHeight / 2)}px`
     });
 }
 
@@ -11,7 +11,7 @@ function player2Comands() {
     let ballPositionY = ball.getBoundingClientRect().top
     setInterval(() => {
         ballPositionY = ball.getBoundingClientRect().top
-        player2.style.top = `${ballPositionY}px`
+        player2.style.top = `${ballPositionY - Number(player2.clientHeight / 2)}px`
     }, 100)
 }
 
